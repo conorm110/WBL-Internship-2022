753910fba4f48d736503a004d97091da9a84554d
amschmidt2 <aebill14@gmail.com>
2022-06-29 14:15:34 -0500

    I cant think of a message worked on a bit of things :)

--- DanceBot - JAVA/src/main/java/frc/robot/Robot.java
+++ DanceBot - JAVA/src/main/java/frc/robot/Robot.java
@@ -6,20 +6,22 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj.DigitalInput;
+// import edu.wpi.first.wpilibj.Joystick;
+// import edu.wpi.first.wpilibj.motorcontrol.MotorController;
+// import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 //import edu.wpi.first.wpilibj.drive.DifferentialDrive.WheelSpeeds;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
+//import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
 //import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
+import edu.wpi.first.wpilibj.motorcontrol.PWMSparkMax;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import com.revrobotics.CANSparkMax;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 
 
@@ -38,43 +40,94 @@ public class Robot extends TimedRobot {
 
   private final XboxController joy0 = new XboxController(0);
 
+  String axis_list[] = {"l_stick_x", "l_stick_y", "l_trig", "r_trig", "r_stick_x", "r_stick_y"};
+  
   Timer timmy = new Timer();
   Wheels wally = new Wheels("wally", .7);
   Auto archie = new Auto("archie");
+  Limelight lucy = new Limelight("lucy");
+  Driver driver = new Driver("driver");
+
+  public class Driver{
+    private String name;
+    private XboxController joy;
+
+    public Driver(String name){
+      this.name = name;
+    }
+
+    public double get_axis(int raw_axis){
+      return joy.getRawAxis(raw_axis);
+    }
+  }
+
 
   public class Wheels{
     private String name;
-    private CANSparkMax front_LeftMotor = new CANSparkMax(1, MotorType.kBrushless);
-    private CANSparkMax back_LeftMotor = new CANSparkMax(2, MotorType.kBrushless);
-    private CANSparkMax front_RightMotor = new CANSparkMax(3, MotorType.kBrushless);
-    private CANSparkMax back_RightMotor = new CANSparkMax(4, MotorType.kBrushless);
+    private double max_speed;
+    private double max_turn = 0.4;
 
-    private MotorControllerGroup ralf = new MotorControllerGroup(front_RightMotor, back_RightMotor);
-    private MotorControllerGroup louie = new MotorControllerGroup(front_LeftMotor, back_LeftMotor);
+    private PWMSparkMax ralf = new PWMSparkMax(0);
+    private PWMSparkMax louie = new PWMSparkMax(1);
 
-    private final DifferentialDrive drivechain = new DifferentialDrive(louie, ralf);
+   // private CANSparkMax front_LeftMotor = new CANSparkMax(1, MotorType.kBrushless);
+   // private CANSparkMax back_LeftMotor = new CANSparkMax(2, MotorType.kBrushless);
+   // private CANSparkMax front_RightMotor = new CANSparkMax(3, MotorType.kBrushless);
+    //private CANSparkMax back_RightMotor = new CANSparkMax(4, MotorType.kBrushless);
+
+    //private MotorControllerGroup ralf = new MotorControllerGroup(front_RightMotor, back_RightMotor);
+    //private MotorControllerGroup louie = new MotorControllerGroup(front_LeftMotor, back_LeftMotor);
+
+    private final DifferentialDrive drivechain = new DifferentialDrive(ralf, louie);
     public double[] shush = {0.0, 0.0};
 
-    public Wheels(String name, double max_speed){
+    public Wheels(String name, double _max_speed){
       this.name = name;
-      max_speed = max_speed;
+      max_speed = _max_speed;
       ralf.setInverted(true);
     }
 
-    private void wheels(double speed, double turn){
+    private void check(double speed, double turn){
+      sensitive(speed, turn);
+    }
+
+    private void drive(double speed, double turn){
       drivechain.arcadeDrive(speed, -turn);
+    }
+
+    private void sensitive(double speed, double raw_turn){
+      double turn = Math.pow(raw_turn, 2.0);
+      if (raw_turn < 0){
+        turn = -turn;
+      }
+
+      speed = max_speed * speed;
+      turn = max_turn * turn;
+
+      drive(speed, turn);
+
     }
 
     public void auto(double speed, double turn){
       shush[0] = speed;
       shush[1] = turn;
-      
+    }
+
+    public void driving(){
+      drive(shush[0], shush[1]);
     }
 
   }
 
 
   public class Auto{
+
+    private String moving [][]={
+      {"Moving forward", "None", "0.1"},
+      {"Moving backwards", "None", "0.3"},
+      {"Moving Left", "None", "0.4"},
+      {"Moving Right", "None", "0.2"},
+    };
 
     private int autonomous_counter = 0;
     private String name;
@@ -93,16 +146,40 @@ public class Robot extends TimedRobot {
     private void briefcase(String task){
       System.out.println(task);
       switch(task){
-        case "Moving forward":
+      case "Moving forward":
         System.out.println("Moving forward");
         wally.auto(-3,0);
         break;
+      case "Moving backwards":
+        System.out.println("Moving back");
+        wally.auto(-0.6, 0);
+        break;
+      case "Moving right":
+        System.out.println("Moving right");
+        wally.auto(0, .4);
+        break;
+      case "Moving left":
+        System.out.println("Moving left");
+        wally.auto(0, -.3);
+        break;
+      case "None":
+        System.out.print("Nothing");
+        break;
+      
+        
       }
     }
 
   }
 
   
+  public class Limelight{
+    private String name;
+
+    public Limelight(String name){
+      this.name = name;
+    }
+  }
 
 
   @Override
@@ -120,12 +197,7 @@ public class Robot extends TimedRobot {
   @Override
   public void teleopPeriodic() {
 
-    double joy_left = joy0.getRawAxis(0);
-    double joy_right = joy0.getRawAxis(1);
-    
-    wheels(joy_left, joy_right);
-
-
+    wally.check(driver.get_axis(1), driver.get_axis(1));
 
   }
 
