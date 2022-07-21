16cc3513f761c6baf5fdda9ffae1d71ceb4e0905
conorm <mjoconnor110@gmail.com>
2022-07-21 16:49:43 -0500

    follows you in teleop

--- conortryinglocoonfrcbot/conortrieslocomotionlmao/src/main/java/frc/robot/subsystems/MirrorDance.java
+++ conortryinglocoonfrcbot/conortrieslocomotionlmao/src/main/java/frc/robot/subsystems/MirrorDance.java
@@ -8,17 +8,51 @@ public class MirrorDance extends SubsystemBase {
     
   }
 
+  private static double target_y_angle = 3;
+  private static double y_angle_tolerance = 2;
+  private static double target_x_angle = -12; // needs to be changed a bit, needed bc limelight isnt centered
+  private static double x_angle_tolerance = 5;
+  private static double speed = 4.20;
+
   public static void FollowPlayer(LucyLimeLight lucy)
   {
     lucy.check();
-    if (lucy.lucy_angles[0] == 0) {
-        Movement.stop();
-      } 
-      else if (lucy.lucy_angles[0] < -12) {
-        Movement.leftJaguar.setVoltage(-3);
-      } 
-      else if (lucy.lucy_angles[0] > -12) {
-        Movement.rightJaguar.setVoltage(3);
-      }
+    double left_voltage = 0;
+    double right_voltage = 0;
+    if ((-1*y_angle_tolerance) < lucy.lucy_angles[1] && y_angle_tolerance > lucy.lucy_angles[1])
+    {
+        // in good y position
+    }
+    else if (lucy.lucy_angles[1] < target_y_angle)
+    {
+        left_voltage = speed;
+        right_voltage = -1 * speed;
+    }
+    else 
+    {
+        left_voltage = -1 * speed;
+        right_voltage = speed;
+    }
+
+    if ((-1*x_angle_tolerance) < lucy.lucy_angles[0] && x_angle_tolerance > lucy.lucy_angles[0])
+    {
+        // in good x position
+    }
+    else if (lucy.lucy_angles[0] < target_x_angle)
+    {
+        left_voltage -= 3;
+        right_voltage -= 3;
+    }
+    else 
+    {
+        left_voltage += 3;
+        right_voltage += 3;
+    }
+
+
+
+    Movement.leftJaguar.setVoltage(left_voltage);
+    Movement.rightJaguar.setVoltage(right_voltage);
+
   }
 }
